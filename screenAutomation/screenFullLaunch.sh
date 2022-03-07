e0c7ab83df86a015a66931532c6627ceb8c0627e
alc9 <alexanderlewiscunningham@gmail.com>
2022-03-07 16:45:53 +0000

    tested sunfounder servo, added ina to gnuscreen automation

--- screenAutomation/screenFullLaunch.sh
+++ screenAutomation/screenFullLaunch.sh
@@ -2,6 +2,7 @@
 screen -dmS roscoreScreen bash -c "cd /home/stingray/stingray_ws; roscore; exec bash -i"
 screen -dmS serverScreen bash -c "cd /home/stingray/stingray_ws; roslaunch action_server as_launch.launch; exec bash -i"
 screen -dmS pysensorScreen bash -c "cd /home/stingray/stingray_ws/src/pysensors/scripts; python3 py_sensors.py; exec bash -i"
+screen -dmS inaScreen bash -c "cd /home/stingray/stingray_ws/src/pysensors/scripts; python3.7 ina_sensor.py; exec bash -i"
 screen -dmS safetyScreen bash -c "cd /home/stingray/stingray_ws; roslaunch safety_action_client sac_launch.launch; exec bash -i"
 if [ $# -eq 0 ]
 then
--- src/depth_control/scripts/testSunfounder.py
+++ src/depth_control/scripts/testSunfounder.py
@@ -1,18 +1,59 @@
 #!/usr/bin/env python
 import rospy
-import time
 from std_msgs.msg import Float32
 from std_msgs.msg import Bool
 from SunFounder_PCA9685.Servo import Servo
+noseServo=Servo(1,address=0x41,bus_number=1)
+noseServo.setup()
+noseServo.write(90.0)
+import numpy as np
+def test(a):
+	'''Servo driver test on channel 1'''
+	import time
+	for i in range(0, 180, 5):
+		print(i)
+		a.write(i)
+		time.sleep(0.1)
+	for i in range(180, 0, -5):
+		print(i)
+		a.write(i)
+		time.sleep(0.1)
+	for i in range(0, 91, 2):
+		a.write(i)
+		time.sleep(0.05)
+	print(i)
+def testb(s):
+    import time
+    data = np.arange(-27,27.5,0.5).tolist()
+    posList=[]
+    start = time.time()
+    print(data)
+    for d in data:
+        angle = 90.0 + (90.0/27.0) * d
+        posList.append(angle)
+    while time.time()-start < 10:
+        print("forward")
+        for p in posList:
+            print(p)
+            s.write(p)
+            time.sleep(0.08)
+        print("reversed")
+        for p in reversed(posList):
+            print(p)
+            s.write(p)
+            time.sleep(0.08)
+testb(noseServo)
+print("resetting to 90 degrees")
+noseServo.write(90.0)
 """
 Third PCA9685 driver in the system which should allow for easy power delivery and higher quality waves
+"""
 """
 class DepthController():
     def __init__(self):
         self.nose_angle_=90.0
         #TODO: solder pin for 0x42
-        self.nose_servo_=Servo(1,address=0x42,bus_number=1)
-        self.nose_servo_.setup()
+        self.nose_servo_=Servo(1,address=0x42)
         self.pause_flag_=False
         self.nose_sub_=rospy.Subscriber("stingray/control/nose_angle",Float32,callback=self.noseSubCb,queue_size=1)
         self.pause_flag_sub_=rospy.Subscriber("stingray/control/pause_flag",Bool,callback=self.pauseFlagSubCb,queue_size=1)
@@ -23,7 +64,7 @@ class DepthController():
             print("Error nose angle out of range: ",data)
             return
         #convert nose angle to servo angle
-        self.nose_angle_=90.0+((90.0/26.0)*data)
+        self.nose_angle_=90.0+((90.0//26.0)*data)
 
     def pauseFlagSubCb(self,data):
         self.pause_flag_=data.data
@@ -33,7 +74,6 @@ class DepthController():
     
     def actuate(self):
         servo.write(self.nose_angle_)
-        time.sleep(0.01)
 def main():
     rospy.init_node("depth_controller_node")
     depthController=depthController()
@@ -44,3 +84,4 @@ def main():
 
 if __name__=="__main__":
     main()
+"""    
